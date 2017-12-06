5f2017759dfb0cb5218c683f7a81886022b45c48
qiuhaoxin <502967244@qq.com>
2017-12-07 00:16:50 +0800

    first commit

---
+++ public/js/versionCheck.js
@@ -0,0 +1,17 @@
+//版本检测
+
+var chalk=require('chalk');
+var semver=require('semver');
+var packageConfig=require('../package.json');
+
+var shell=require('shelljs');
+function exec(cmd){
+	return require('child_process').execSync(cmd).toString().trim();
+}
+var versionRequirements=[
+     {
+     	name:'node',
+     	currentVersion:semver.clean(process.version);
+     	versionRequirements:packageConfig.engines.node
+     },
+]
---
+++ server/app.js
@@ -0,0 +1,7 @@
+/*
+* @author haoxin_qiu
+*/
+var express=require('express');
+var path=require('path');
+var favicon=require('serve-favicon');
+var logger=require('morgan');
