4d554355245e80a2743bfe4b682978657e85746a
qiuhaoxin <502967244@qq.com>
2017-12-12 21:02:35 +0800

    c

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,5 +1,6 @@
 var queryString=require('querystring');
 var url=require('url');
+var md5=require('md5.js');
 function Utility(){
 
 
@@ -11,6 +12,10 @@ Utility.prototype={
         	return true;
         }
         return false;
+	},
+	md5Encode:function(str){
+        var encodeResult=new MD5().update(str).digest('hex');
+        console.log("encodeResult is "+encodeResult);
 	}
 };
 module.exports=new Utility();
