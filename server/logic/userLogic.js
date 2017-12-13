384a2d91dc576fc4364cbecc8ed1bbdf7ce31410
qiuhaoxin <502967244@qq.com>
2017-12-13 17:18:11 +0800

    c

--- server/logic/userLogic.js
+++ server/logic/userLogic.js
@@ -8,7 +8,7 @@
 
  var pool=mysql.createPool(sqlConfig['sqlConfig']);
 
-
+ //数据库执行结果统一返回格式：{data:[],result:'',code:0} code:   1:执行数据查询成功！ -1.执行数据查询失败
  var userLogic={
  	//执行SQL
  	execSql:function(sql,params,cb){
@@ -21,7 +21,7 @@
          } 	 
          usePsw= Utility.md5Encode(usePsw);
          console.log("userPsw is "+usePsw);
-         if(this._isExitPerson(userName)){
+         if(this.searchPerson(userName)){
             this.execSql("select 1 as result from t_person where FName=? and FPsw=?",[userName,usePsw],cb);
          }else{
          	return {mes:'用户名不存在！'};
@@ -29,7 +29,7 @@
 
  	},
  	//同一个组织下是否已存在相同的用户，以手机/登陆名为标准
- 	_isExitPerson:function(mobile){
+ 	searchPerson:function(mobile){
          this.execSql("select 1 as result from t_person where FName=? or FMobile=?",[mobile],function(error,result){
              if(error)throw error;
              console.log("result is "+JSON.stringify(result));
@@ -46,7 +46,27 @@
  		if(Utility.isEmpty(params.FPsw)){
  			return {mes:'密码不能为空!'};
  		}
+
  		this.execSql("insert into t_person set ?",params,cb)
+ 	},
+ 	//删除用户
+ 	deleteUser:function(mobile){
+       if(Utility.isEmpty(mobile)){
+       	   return {result:"请输入删除的用户名或手机号码！",data:[],code:-2};
+       }
+
+ 	},
+ 	//获取当前组织下的所有用户
+ 	getUserList:function(){
+
+ 	},
+ 	//获取部门
+ 	getDeptList:function(){
+
+ 	},
+ 	//修改密码
+ 	changePsw:function(mobile,newPsw,oldPsw){
+
  	}
  }
  module.exports=userLogic;
