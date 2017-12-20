6757f446be7dcb90cf17b668125f67ccab991827
qiuhaoxin <502967244@qq.com>
2017-12-21 00:04:37 +0800

    c

--- web/pages/main/mainPage.vue
+++ web/pages/main/mainPage.vue
@@ -5,6 +5,10 @@
                 <div>{{companyName}}</div><div><span>{{userName}}</span><span>退出</span></div>
             </div>
             <div class='header-bottom'>
+                <ul>
+                   <li></li>
+                </ul>
+
             </div>
         </header>
    </section>
@@ -17,15 +21,24 @@
                userName:'邱浩新',
 
            }
+         },
+         created(){
+            console.log("hei");
+         },
+         methods:{
+
          }
      }
 </script>
 <style lang='scss' scoped>
    @import '../../style/mixin';
    header{
-       height:100px;background:#036;@include flexJA();color:#fff;
+       height:100px;background:#036;@include flexJA();color:#fff;flex:1px 1 1;
    }
    .header-top{
-       @include flexJA(row,center);width:100%;
+       @include flexJA(row,center);width:100%;flex-grow:1
+   }
+   .header-bottom{
+       flex-grow:1
    }
 </style>
